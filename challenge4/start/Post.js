f188b1259a68f4a7aba7bccd3bf0a541adc84e35
sherchans <sudip.sherchan@ilmservice.com>
2015-06-10 09:24:26 -0500

    fixt

--- challenge4/start/Post.js
+++ challenge4/start/Post.js
@@ -2,7 +2,7 @@
 var mongoose = require('mongoose');
 
 // Configure conenction URL (only needs to happen once per app)
-mongoose.connect('CHANGEME');
+mongoose.connect('mongodb://localhost:27017/blogpost');
 
 // Create a database schema for our Post object, which will describe both it's
 // data and it's behavior.
